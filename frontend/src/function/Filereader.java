5f2541c339ce865e0a08598097911f4b3c731638
LianD <Lian.Duan@Alumni.ubc.ca>
2019-01-26 21:38:03 -0800

    File reader

---
+++ frontend/src/function/Filereader.java
@@ -0,0 +1,21 @@
+package function;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Filereader {
+
+    private String readFile(String pathname) throws IOException {
+
+        File file = new File(pathname);
+        StringBuilder fileContents = new StringBuilder((int)file.length());
+
+        try (Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine() + System.lineSeparator());
+            }
+            return fileContents.toString();
+        }
+    }
+}
--- frontend/src/function/StringParse.java
+++ frontend/src/function/StringParse.java
@@ -1,4 +1,25 @@
 package function;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class StringParse {
+    public String transcribedData;
+
+    public String parseData() {
+        String json = transcribedData;
+        List<String> extracteddata = new ArrayList<>();
+        String parts[] = json.split(",");
+        String transcriptExtract = parts[1];
+
+        String parts2[] = transcriptExtract.split(":");
+        transcriptExtract = parts2[2];
+
+        transcriptExtract = transcriptExtract.replaceAll("\"", "").trim();
+        System.out.println("you said: " + transcriptExtract);
+
+        return transcriptExtract;
+
+    }
 }
