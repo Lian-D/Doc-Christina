355ae4bd5df5fe3755ad200b4a7627b6d7e69d10
LianD <Lian.Duan@Alumni.ubc.ca>
2019-01-26 21:20:46 -0800

    priority updated

--- frontend/src/function/PriorityList.java
+++ frontend/src/function/PriorityList.java
@@ -3,5 +3,14 @@ package function;
 import java.util.ArrayList;
 
 public class PriorityList {
+    public ArrayList<Alert> priorityAlerts;
     public ArrayList<Alert> alerts;
+
+    //Return a list of alerts in the order of priority
+    public ArrayList<Alert> getAlerts() {
+        ArrayList<Alert> finalList = new ArrayList<>();
+        finalList.addAll(priorityAlerts);
+        finalList.addAll(alerts);
+        return  finalList;
+    }
 }
