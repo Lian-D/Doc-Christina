b03a21f8426bc68d6b32e47e21076ce0154f7494
LianD <Lian.Duan@Alumni.ubc.ca>
2019-01-27 02:59:40 -0800

    KURISIIIIINA~

--- frontend/src/gui/Main.java
+++ frontend/src/gui/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-        primaryStage.setTitle("doc: Hospital Alert");
+        primaryStage.setTitle("Christina: Hospital Alert");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
